77d5f2d86a6f874055198598b15ca33bde2f433d
Fmolinan <felipe.molinan@usach.cl>
2020-04-03 13:05:01 -0300

    Updates

---
+++ precache-manifest.c208298ecbb29f3385f2a3001e1f6716.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cc3749dcabeabe5d7fcdeaa933b6d7f",
+    "url": "/fosfoquim/index.html"
+  },
+  {
+    "revision": "e20738574a0e1dd953d1",
+    "url": "/fosfoquim/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "241180507ba3a9b6b50f",
+    "url": "/fosfoquim/static/css/main.33289faa.chunk.css"
+  },
+  {
+    "revision": "e20738574a0e1dd953d1",
+    "url": "/fosfoquim/static/js/2.e37559b8.chunk.js"
+  },
+  {
+    "revision": "d14f21e42c09509e44bd25febcf4b2dc",
+    "url": "/fosfoquim/static/js/2.e37559b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241180507ba3a9b6b50f",
+    "url": "/fosfoquim/static/js/main.3869324c.chunk.js"
+  },
+  {
+    "revision": "eec627bda7caeedfd15f",
+    "url": "/fosfoquim/static/js/runtime-main.90b13bb5.js"
+  },
+  {
+    "revision": "1a26c22e3901cf254c6434290672aa15",
+    "url": "/fosfoquim/static/media/accounts-active.1a26c22e.svg"
+  },
+  {
+    "revision": "0b9339193e1975bafb3828d331231ac1",
+    "url": "/fosfoquim/static/media/calendar.0b933919.svg"
+  },
+  {
+    "revision": "2b36db2f100833db6fffec1f2051efe7",
+    "url": "/fosfoquim/static/media/chip.2b36db2f.svg"
+  },
+  {
+    "revision": "55a6fa733138d130e07798c727de72ab",
+    "url": "/fosfoquim/static/media/clients-active.55a6fa73.svg"
+  },
+  {
+    "revision": "f045f0f6acdf85714c36b5809a005713",
+    "url": "/fosfoquim/static/media/device-active.f045f0f6.svg"
+  },
+  {
+    "revision": "e840bd6af15cee2d92a6baca0740d691",
+    "url": "/fosfoquim/static/media/down-arrow.e840bd6a.svg"
+  },
+  {
+    "revision": "9501fe4d9ded800b9ca0d4580d940268",
+    "url": "/fosfoquim/static/media/gear.9501fe4d.svg"
+  },
+  {
+    "revision": "38e28fdcb4475a22684948c154eac19a",
+    "url": "/fosfoquim/static/media/logo.38e28fdc.svg"
+  },
+  {
+    "revision": "b6890bc9c95de59e1da270a7c39b31b9",
+    "url": "/fosfoquim/static/media/logout.b6890bc9.svg"
+  },
+  {
+    "revision": "51c44790b31f99759ffa25c30cca8687",
+    "url": "/fosfoquim/static/media/mail.51c44790.svg"
+  },
+  {
+    "revision": "abec3eda07744fe5c4ac3bcb1aeac333",
+    "url": "/fosfoquim/static/media/menu.abec3eda.svg"
+  },
+  {
+    "revision": "c749c64966ea5e6251869bcbc69b9674",
+    "url": "/fosfoquim/static/media/search.c749c649.svg"
+  },
+  {
+    "revision": "f5936394f01b64c7093e5fa346a72ad8",
+    "url": "/fosfoquim/static/media/team.f5936394.svg"
+  }
+]);
